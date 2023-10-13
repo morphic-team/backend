7a6a87f47780f9cbfaf0012b913d335777995954
Pierre Hugo <2152709+avoid3d@users.noreply.github.com>
2023-10-13 20:26:27 +0200

    Add background work and sentry

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,30 +1,42 @@
 import os
+import sentry_sdk
 from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+sentry_sdk.init(
+    dsn="https://9eeb4684dc1ab49e736b24f3bbc8c789@sentry.morphs.io/2",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
+
+
 def add_cors(response):
-  response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin', '*')
-  response.headers['Access-Control-Allow-Credentials'] = 'true'
-  response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization,Morphs-Handle-Errors-Generically'
-  response.headers['Access-Control-Allow-Methods'] = 'PUT,DELETE,PATCH'
-  return response
-
-def create_app(environment='dev'):
-  app = Flask(__name__)
-
-  # TODO: Fix this hardcoded password.
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
-  app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers[
+        "Access-Control-Allow-Headers"
+    ] = "Content-Type, Authorization,Morphs-Handle-Errors-Generically"
+    response.headers["Access-Control-Allow-Methods"] = "PUT,DELETE,PATCH"
+    return response
 
 
-  db.init_app(app)
+def create_app(environment="dev"):
+    app = Flask(__name__)
 
-  from backend.api import api
-  app.after_request(add_cors)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = "False"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
+    app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
-  app.register_blueprint(api, url_prefix="/api")
+    db.init_app(app)
 
-  return app
+    from backend.api import api
+
+    app.after_request(add_cors)
+
+    app.register_blueprint(api, url_prefix="/api")
+
+    return app
